04127693fe96f59489f172905178561d75e60dc3
FangWei12 <63558071+FangWei12@users.noreply.github.com>
2020-04-19 20:58:26 +0000

    My Second Project Week 13

---
+++ MySecondProject/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {  Text, TextInput, View } from 'react-native';
+
+ function MyApp() {
+  return (
+    <View>
+      <Text>
+
+        Hello, I am a student in CIS340! 
+       </Text>
+
+  </View>
+  );
+}
+
+
+export default function MultiComp(){
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}
+    >
+      <Text> {"\n\n\n\n\n\n\n"} Welcome to My class </Text>
+               <MyApp />
+               <MyApp />
+               <MyApp />
+               <MyApp />
+
+      </View>
+  )
+}
